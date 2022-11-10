ea129dad4349e458ca8b7bf534577869dec2b208
guztus <gustavs.zubars@gmail.com>
2022-11-10 15:23:23 +0200

    added correct index file

--- base game/index.php
+++ base game/index.php
@@ -24,29 +24,34 @@ $scissors->setBeats($paper, $lizard);
 $spock->setBeats($rock, $scissors);
 $lizard->setBeats($paper, $spock);
 
+$player1Name = (string)readline("Enter your name: ");
+while (true) {
+    $playAgain = readline("Ready to play? (y/n) ");
+    if (strtolower($playAgain) !== 'y') {
+        exit;
+    }
 
-$player1Name = (string) readline("Enter your name: ");
-echo "Available elements:" . PHP_EOL;
-foreach ($elements as $key => $element) {
-    echo "[{$key}] {$element->getName()}" . PHP_EOL;
+    echo "Available elements:" . PHP_EOL;
+    foreach ($elements as $key => $element) {
+        echo "[{$key}] {$element->getName()}" . PHP_EOL;
+    }
+
+
+    $player1Element = (int)readline("Choose your element [by number]: ");
+    $player1 = new Player($player1Name);
+    $player1->setElement($elements[$player1Element]);
+
+    $player2Element = $elements[array_rand($elements)];
+    $player2 = new Player('Computer');
+    $player2->setElement($player2Element);
+
+
+    $game = new Game($player1, $player2);
+
+    $winner = $game->getWinner();
+    if ($winner == null) {
+        echo "The game is a tie!" . PHP_EOL;
+        continue;
+    }
+    echo "{$winner->getName()} wins with {$winner->getElement()->getName()}" . PHP_EOL . PHP_EOL;
 }
-
-
-$player1Element = (int) readline("Choose your element [by number]: ");
-$player1 = new Player($player1Name);
-$player1->setElement($elements[$player1Element]);
-
-$player2Element = $elements[array_rand($elements)];
-$player2 = new Player('Computer');
-$player2->setElement($player2Element);
-
-
-$game = new Game($player1, $player2);
-
-$winner = $game->getWinner();
-if ($winner == null) {
-    echo "The game is a tie!" . PHP_EOL;
-    exit;
-}
-echo "{$winner->getName()} wins with {$winner->getElement()->getName()}" . PHP_EOL;
-exit;
